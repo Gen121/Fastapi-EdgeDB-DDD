a38212964e5633b8d48f20e6bba74745d0c293c3
Gen121 <chelnok1190@gmail.com>
2023-06-11 21:43:55 -0300

    Add fixture client for edge db

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from db_tables import metadata
-
+import edgedb
 
 @pytest.fixture
 def in_memory_db():
@@ -16,3 +16,9 @@ def in_memory_db():
 @pytest.fixture
 def session(in_memory_db):
     yield sessionmaker(bind=in_memory_db)()
+
+
+@pytest.fixture
+def client():
+    return edgedb.create_client()
+
