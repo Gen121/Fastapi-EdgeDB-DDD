796fb73a046b6e92a030084e2b517b4e66a5d2f6
Gen121 <chelnok1190@gmail.com>
2023-06-29 00:42:37 -0300

    Changes in repository

--- repository.py
+++ repository.py
@@ -1,17 +1,16 @@
 import abc
 from uuid import UUID
 import edgedb
-import pyd_model
-import model
+import pyd_model as model
 
 
 class AbstractRepository(abc.ABC):
     @abc.abstractmethod
-    def add(self, batch: pyd_model.Batch):
+    def add(self, batch: model.Batch):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get(self, reference) -> pyd_model.Batch:
+    def get(self, reference) -> model.Batch:
         raise NotImplementedError
 
 
@@ -19,43 +18,81 @@ class EdgeDBRepository(AbstractRepository):
     def __init__(self, client):
         self.client: edgedb.Client = client
 
-    def get(self, key: UUID | str) -> pyd_model.Batch:
-        if isinstance(key, UUID):
-            return self.get_batch_by_id(key)
-        return self.get_batch_by_reference(key)
+    def get(self, uuid: UUID | None = None, reference: str | None = None) -> model.Batch:
+        """Return Batch by UUID or Reference."""
+        if not any((uuid, reference)):
+            raise Exception('Необходим UUID или reference')
+
+        obj_ = self.client.query_required_single(
+            """select Batch {**}
+                filter .id ?= <optional uuid>$uuid
+                or .reference ?= <optional str>$reference
+                LIMIT 1
+            """,
+            uuid=uuid, reference=reference)
+        return model.Batch.from_orm(obj_)
 
     def add(self, batch: model.Batch) -> None:
         return self.add_batch(batch)
 
     def add_batch(self, batch: model.Batch) -> None:
+        batch.allocations = list(batch.allocations)  # noqa
         self.client.query(
-            """
-            INSERT Batch {
-                purchased_quantity := <int16>$purchased_quantity,
-                eta := <cal::local_date>$eta,
-                reference := <str>$reference,
-                sku := <str>$sku,
+            """with
+            obj := <json>$data,
+            list_orders := <array<json>>obj['allocations'] ?? [<json>{}],
+            new_batch := (insert Batch {
+                reference := <str>obj['reference'],
+                sku := <str>obj['sku'],
+                eta := <cal::local_date>obj['eta'],
+                purchased_quantity := <int16>obj['purchased_quantity'],
             }
+            unless conflict on .reference else (
+                update Batch set {
+                reference := <str>obj['reference'],
+                sku := <str>obj['sku'],
+                eta := <cal::local_date>obj['eta'],
+                purchased_quantity := <int16>obj['purchased_quantity'],
+                }
+            )),
+            for order_line in array_unpack(list_orders) union (
+                select (
+                insert OrderLine {
+                    orderid := <str>order_line['orderid'],
+                    qty := <int16>order_line['qty'],
+                    sku := <str>order_line['sku'],
+                    allocated_in := new_batch
+                } unless conflict on .orderid else (
+                    update OrderLine
+                    set {
+                        orderid := <str>order_line['orderid'],
+                        qty := <int16>order_line['qty'],
+                        sku := <str>order_line['sku'],
+                        allocated_in := new_batch
+                    }
+                )
+                )
+            );
+            with obj := <json>$data,
+            select Batch filter .reference = <str>obj['reference'];
             """,
-            purchased_quantity=batch._purchased_quantity,
-            eta=batch.eta,
-            reference=batch.reference,
-            sku=batch.sku,
+            data=batch.json()
         )
+        batch.allocations = set(batch.allocations)
 
-    def get_batch_by_reference(self, reference: str) -> pyd_model.Batch:
+    def get_batch_by_reference(self, reference: str) -> model.Batch:
         batch = self.client.query_required_single_json(
             "SELECT Batch {**} FILTER .reference = <str>$reference LIMIT 1",
             reference=reference,
         )
-        return pyd_model.Batch.parse_raw(batch)
+        return model.Batch.parse_raw(batch)
 
-    def get_batch_by_id(self, id: UUID) -> pyd_model.Batch:
+    def get_batch_by_id(self, id: UUID) -> model.Batch:
         batch = self.client.query_required_single_json(
             "SELECT Batch {**} FILTER .id = <uuid>$id LIMIT 1",
             id=id,
         )
-        return pyd_model.Batch.parse_raw(batch)
+        return model.Batch.parse_raw(batch)
 
 
 class FakeRepository(AbstractRepository):
