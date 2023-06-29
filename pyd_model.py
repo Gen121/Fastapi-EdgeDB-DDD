f8e8ec79772d82eb7d8233b24435f620c9b14ef7
Gen121 <chelnok1190@gmail.com>
2023-06-29 00:36:53 -0300

    Changes in pyd_models

--- pyd_model.py
+++ pyd_model.py
@@ -3,40 +3,64 @@ from __future__ import annotations
 from datetime import date
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 import model
+from model import OutOfStock  # noqa
 
 
 class OrderLine(BaseModel, model.OrderLine):
-    # primary_key=True, autoincrement=True
-    id: UUID | None = Field(default=None, exclude=True)
-    sku: str  # String(255)
-    qty: int  # Integer, nullable=False
-    orderid: str  # String(255)
+    id: UUID | None = Field(default=None)
+    sku: str
+    qty: int
+    orderid: str
 
     def __hash__(self):
         return hash((self.sku, self.qty, self.orderid))
 
+    def __eq__(self, other):
+        if not isinstance(other, model.OrderLine):
+            return False
+
+        return all((
+            other.orderid == self.orderid,
+            other.sku == self.sku,
+            other.qty == self.qty,
+        ))
+
+    class Config:
+        orm_mode = True
+        frozen = True
+
 
 class OrderLineWithAllocatedIn(OrderLine):
-    allocted_in: Batch  # noqa
+    allocated_in: Batch
 
 
 class Batch(BaseModel, model.Batch):
-
-    # primary_key=True, autoincrement=True
-    id: UUID | None = Field(default=None, exclude=True)
-    reference: str  # String(255)
-    sku: str  # String(255)
-    purchased_quantity: int  # Integer, nullable=False
-    eta: date | None  # Date, nullable=True
-    allocations: set[OrderLine] | None = None
+    id: UUID | None = Field(default=None)
+    reference: str
+    sku: str
+    eta: date | None
+    purchased_quantity: int
+    allocations: set[OrderLine | None] = Field(default_factory=set)
 
     def __eq__(self, other):
         if not isinstance(other, Batch):
             return False
         return other.reference == self.reference
+
+    class Config:
+        orm_mode = True
+        json_encoders = {
+            set: list
+        }
+
+    @validator('allocations', pre=True)
+    def convert_to_set(cls, value):
+        if isinstance(value, list):
+            return set((OrderLine.from_orm(obj) for obj in value))
+        return value
 
 
 OrderLineWithAllocatedIn.update_forward_refs()
