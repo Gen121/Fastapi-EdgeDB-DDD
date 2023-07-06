0bd2ba69a0d752965746446df319484c5fe0ef0c
Gen121 <chelnok1190@gmail.com>
2023-07-06 14:18:51 -0300

    Added services and their testing

--- services.py
+++ services.py
@@ -1,19 +1,21 @@
 from pyd_model import OrderLine
 from repository import AbstractRepository
+import model
 
 
 class InvalidSku(Exception):
     pass
 
 
-def is_valid_sku(sku, batches):
+async def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
-    batches = repo.list()
-    if not is_valid_sku(line.sku, batches):
-        raise InvalidSku(f'Недопустимый артикул {line.sku}')
+async def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+    batches = await repo.list()
+    if not await is_valid_sku(line.sku, batches):
+        raise InvalidSku(f'Invalid sku {line.sku}')
     batchref = model.allocate(line, batches)
-    session.commit()
+    [current_batch] = [b for b in batches if b.reference == batchref]
+    await repo.add(current_batch)
     return batchref
---
+++ test_services.py
@@ -0,0 +1,32 @@
+import pytest
+import model
+import repository
+import services
+
+
+class FakeSession:
+    committed = False
+
+    def commit(self):
+        self.committed = True
+
+
+async def test_returns_allocation():
+    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
+    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
+    repo = repository.FakeRepository([batch])
+
+    result = await services.allocate(line, repo, FakeSession())
+    assert result == "b1"
+
+
+async def test_error_for_invalid_sku():
+    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
+    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
+    repo = repository.FakeRepository([batch])
+
+    try:
+        await services.allocate(line, repo, FakeSession())
+    except services.InvalidSku as e:
+        with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+            raise e
