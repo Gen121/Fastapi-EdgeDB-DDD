34a22d80bc642f0d23fe48aa1a51148e60100426
Gen121 <chelnok1190@gmail.com>
2023-08-02 16:38:36 -0300

    Add Events for BatchCreated and AllocationRequired

--- src/allocation/domain/events.py
+++ src/allocation/domain/events.py
@@ -1,10 +1,26 @@
 from dataclasses import dataclass
+from datetime import date
 
 
-class Event():
+class Event:
     pass
 
 
 @dataclass
 class OutOfStock(Event):
     sku: str
+
+
+@dataclass
+class BatchCreated(Event):
+    ref: str
+    sku: str
+    qty: int
+    eta: date | None = None
+
+
+@dataclass
+class AllocationRequired(Event):
+    orderid: str
+    sku: str
+    qty: int
